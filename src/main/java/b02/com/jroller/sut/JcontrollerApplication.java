d6329fef85b7267b77503ada5a1e778e754ead33
bekay-perf <67225924+bekay-perf@users.noreply.github.com>
2020-06-21 22:32:15 +0900

    SUT init

---
+++ src/main/java/b02/com/jroller/sut/JcontrollerApplication.java
@@ -0,0 +1,13 @@
+package b02.com.jroller.sut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcontrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcontrollerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/b02/com/jroller/sut/JcontrollerApplicationTests.java
@@ -0,0 +1,13 @@
+package b02.com.jroller.sut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcontrollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
